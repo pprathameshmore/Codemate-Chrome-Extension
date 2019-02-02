5c47cf0f3bf8023024f23ac65ed44dd439abba5c
Prathamesh More <moreprathameshmore@hotmail.com>
2019-02-02 11:04:42 +0530

    Fix broken code

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,35 +1,37 @@
 window.addEventListener('load', function () {
     console.log('All assets are loaded')
+    var searchButton = document.getElementById("search-btn");
+
+    searchButton.addEventListener("click", function () {
+        var searchQuery = document.getElementById('input-query').value;
+        var searchURL;
+
+        if (document.getElementById('google-rb').checked == true) {
+            if (searchQuery === '') {
+                alert("Blank is question not allowed");
+            } else {
+                searchURL = "https://www.google.com/search?q=" + searchQuery;
+                window.open(searchURL);
+            }
+        }
+
+        if (document.getElementById('stackoverflow-rb').checked == true) {
+            if (searchQuery === '') {
+                alert("Blank is question not allowed");
+            } else {
+                searchURL = "https://stackoverflow.com/search?q=" + searchQuery;
+                window.open(searchURL);
+            }
+        }
+
+        if (document.getElementById('google-stackoverflow-rb').checked == true) {
+            if (searchQuery === '') {
+                alert("Blank is question not allowed");
+            } else {
+                searchURL = "https:/www.google.com/search?q=" + searchQuery + " " + "site:stackoverflow.com";
+                window.open(searchURL);
+            }
+        }
+
+    })
 })
-
-function searchQuestion() {
-    var searchQuery = document.getElementById('input-query').value;
-
-    if (document.getElementById('google-rb').checked == true) {
-        if (searchQuery === '') {
-            alert("Blank is question not allowed");
-        } else {
-            var searchURL = "https://www.google.com/search?q=" + searchQuery;
-            window.open(searchURL);
-        }
-    }
-
-    if (document.getElementById('stackoverflow-rb').checked == true) {
-        if (searchQuery === '') {
-            alert("Blank is question not allowed");
-        } else {
-            var searchURL = "https://stackoverflow.com/search?q=" + searchQuery;
-            window.open(searchURL);
-        }
-    }
-
-    if (document.getElementById('google-stackoverflow-rb').checked == true) {
-        if (searchQuery === '') {
-            alert("Blank is question not allowed");
-        } else {
-            var searchURL = "https:/www.google.com/search?q=" + searchQuery + " " + "site:stackoverflow.com";
-            window.open(searchURL);
-        }
-    }
-
-}
